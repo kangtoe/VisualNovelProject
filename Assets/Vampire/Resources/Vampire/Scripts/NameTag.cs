41e63276b9c99522114045e862fe4a93ef10a4ae
JunHyeong Park <suldangoo@gmail.com>
2023-10-01 15:57:44 +0900

    이름이 있을 때만 네임태그 출력

--- Assets/Vampire/Resources/Vampire/Scripts/NameTag.cs
+++ Assets/Vampire/Resources/Vampire/Scripts/NameTag.cs
@@ -1,16 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using Utage;
 
 public class NameTag : MonoBehaviour
 {
-    void Start()
-    {
-        //gameObject.SetActive(false);
-    }
+    [SerializeField]
+    UguiNovelText text; // 비주얼 노벨 씬의 NameText
+
+    [SerializeField]
+    Image image; // 네임 태그의 image 컴포넌트
 
     void Update()
     {
-        
+        Color color = image.color;
+
+        // 글이 비어있을 경우
+        if (String.IsNullOrEmpty(text.text))
+        {
+            color.a = 0f; // 투명도를 0
+        }
+        else
+        {
+            color.a = 255f; // 투명도를 255
+        }
+
+        image.color = color;
     }
 }
